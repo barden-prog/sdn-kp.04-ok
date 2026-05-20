6552f8830c853c7c08edbc303ab6d6ec1949d4c3
barden <nurman90@admin.sd.belajar.id>
2026-05-21 00:43:57 +0700

    Replace BrowserRouter with HashRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { HashRouter } from 'react-router-dom' // Impor HashRouter
 
-import App from "./App";
-import Pendaftar from "./pendaftar";
-
-import "./index.css";
-
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-    <BrowserRouter>
-
-      <Routes>
-
-        <Route
-          path="/"
-          element={<App />}
-        />
-
-        <Route
-          path="/pendaftar"
-          element={<Pendaftar />}
-        />
-
-      </Routes>
-
-    </BrowserRouter>
-
-  </React.StrictMode>
-);
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+)
